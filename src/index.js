61fafa4be2a061e5b45c735bf74d1982cdbc8805
yang12310 <yang12310@naver.com>
2023-05-06 12:51:56 +0900

    2023-05-06 01 commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+// 스토어를 만듭니다. 매개변수의 key값은 반드시 reducer 이어야 합니다...
+const store = configureStore({
+  reducer : rootReducer,
+},
+  composeWithDevTools()
+);
+console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/modules/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import counter from './counter';
+import todos from './todos';
+
+const rootReducer = combineReducers({
+ counter,
+ todos
+});
+export default rootReducer;
